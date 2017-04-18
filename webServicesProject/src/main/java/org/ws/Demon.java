84a686984d697f7d35ffbde7f8316255ecc75523
wohaha-haha <hwjwjb@sina.com>
2017-04-18 14:54:21 +0800

    mbapp

--- webServicesProject/src/main/java/org/ws/Demon.java
+++ webServicesProject/src/main/java/org/ws/Demon.java
@@ -2,12 +2,24 @@ package org.ws;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.mb.MyMb;
+
 @WebService
 public class Demon {
 
+	private MyMb mb;
 	@WebMethod
 	public String hello(String req)
 	{
-		return req+"=";
+		mb.setName(req);
+		
+		return req+mb.show();
 	}
+	public MyMb getMb() {
+		return mb;
+	}
+	public void setMb(MyMb mb) {
+		this.mb = mb;
+	}
+	
 }
